f74437aee5d74fe81b89131e14796d6e8d9fa9a3
Kyle Aquino <aquino.kylekay@gmail.com>
2019-12-20 18:32:15 -0500

    added skycon.js file to get the icon symbol

--- app.js
+++ app.js
@@ -2,6 +2,9 @@
 window.addEventListener('load', () => {
     let long;
     let lat;
+    let temperatureDescription = document.querySelector('.temperature-description');
+    let temperatureDegree = document.querySelector('.temperature-degree');
+    let locationTimezone = document.querySelector('.location-timezone');
 
     // if the place exist in the broswer, we can find the exact place.
     if (navigator.geolocation) {
@@ -9,6 +12,7 @@ window.addEventListener('load', () => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
 
+            // act as a proxy that allows to request if you are in localhost.
             const proxy = 'http://cors-anywhere.herokuapp.com/';
 
             const api = `${proxy}https://api.darksky.net/forecast/b6ab47245bd009cbb23f58a51784c1f4/${lat},${long}`;
@@ -20,12 +24,26 @@ window.addEventListener('load', () => {
                 })
                 .then(data => {
                     console.log(data);
+                    const { temperature, summary, icon } = data.currently;
+                    //set DOM elemnets from the api
+                    temperatureDegree.textContent = temperature;
+                    temperatureDescription.textContent = summary;
+                    locationTimezone.textContent = data.timezone;
+                    //set icon
+                    setIcons(icon, document.querySelector(".icon"));
+
+
                 });
         });
+    }
 
-
-
-
+    function setIcons(icon, iconID) {
+        const skycons = new Skycons({ color: "white" });
+        //look for every line and replace it with _
+        const currentIcon = icon.replace(/-/g, "_").toUpperCase();
+        skycons.play();
+        //this will add the id and skycons and the current icon.
+        return skycons.set(iconID, Skycons[currentIcon]);
     }
 
 });
